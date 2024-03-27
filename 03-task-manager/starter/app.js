ebe29474926ac80a32993b532ef994bf53c3eb7b
Oksana Fetter <fetter.oksana@gmail.com>
2024-03-27 00:00:39 -0400

    week5

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -18,7 +18,10 @@ const port = 3000
 const start = async () => { 
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(port, console.log (`Server is listening on port ${port}...`))
+        app.listen(
+            port,
+            console.log (`Server is listening on port ${port}...`)
+        )
     } catch (error) {
         console.log(error);
     }
