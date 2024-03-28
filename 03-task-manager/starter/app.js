9f8041a08f893a402cc8dafc628a8e4919b12029
Oksana Fetter <fetter.oksana@gmail.com>
2024-03-28 14:35:42 -0400

    remove json-lock

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,9 +1,9 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-require('dotenv').config();
+
 //middleware
 app.use(express.json());
 
