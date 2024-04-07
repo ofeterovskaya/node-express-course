4fcf8fbc20bb219d725ba41b61b230b2b3cdb2f9
Oksana Fetter <fetter.oksana@gmail.com>
2024-04-07 02:13:37 -0400

    create Input Validation, Error Handling,Additional CRUD Operations

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String, completed:Boolean        
+    name:{
+        type: String,
+        required: [true, 'Please provide a name'],
+        trim: true,
+        maxlength: [20, 'Name cannot be more than 20 characters']   
+    },
+    completed:{
+        type: Boolean,
+        default: false,
+    },        
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
