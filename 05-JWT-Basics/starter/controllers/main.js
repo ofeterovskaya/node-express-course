df22deb189fb25ad2be451d67605947464dafa0f
Oksana Fetter <fetter.oksana@gmail.com>
2024-04-23 20:32:17 -0400

    Add JWT authentication routes and controllers

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,22 @@
+const CustomAPIError = require('../errors/custom-error');
+
+
+const login = async (req, res) => {
+    const {username, password} = req.body;
+    //mongoose validation
+    //Joi
+    //check in the controllers
+    if(!username || !password){
+        throw new CustomAPIError("Please provide email and password", 400);
+}
+   res.send("Fake Login/Register/Signup Route")
+};
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({msg:`Hello, John Doe`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`});
+
+}
+module.exports = {
+    login, dashboard
+}
