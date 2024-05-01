55272f681852a6c1705bc2c263f902e5ef0e580a
Oksana Fetter <fetter.oksana@gmail.com>
2024-05-01 00:49:04 -0400

    Refactor authentication middleware to use optional chaining and improve error handling

--- 05-JWT-Basics/starter/middleware/auth.js
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -1,24 +1,19 @@
-
 const jwt = require ('jsonwebtoken');
 const {UnauthenticatedError} = require('../errors');
 
-
 const authenticationMiddleware = async (req, res, next) => {
-    const authHeader = req.headers.authorization;  
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if(!token){
         throw new UnauthenticatedError('No token provided');
     } 
-const token = authHeader.split(' ')[1];
 
-try { 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);  
-    const {id, username} = decoded;
-    req.user = {id, username};
-    next();
-} catch (error) {
-    throw new UnauthenticatedError('Not authorized');
-}
-    
+    try { 
+        req.user = jwt.verify(token, process.env.JWT_SECRET);
+        next();
+    } catch {
+        throw new UnauthenticatedError('Not authorized');
+    }
 }
 
 module.exports = authenticationMiddleware;
